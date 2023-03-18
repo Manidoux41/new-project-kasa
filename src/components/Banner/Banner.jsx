b94635bc64717a9485318d394e9e5ebdbcdbefc3
Manidoux41 <lherissondu41@gmail.com>
2023-03-18 21:42:19 +0100

    add filter on image

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
+import React from "react";
 
-function Banner({imageSrc, bannerTitle}) {
-
+function Banner({ imageSrc, bannerTitle }) {
   return (
-    <div className='banner'>
+    <div className="banner">
         <img src={imageSrc} alt="imageBanner" />
-        <h2>{bannerTitle}</h2>
+      <h2>{bannerTitle}</h2>
     </div>
-  )
+  );
 }
 
-export default Banner
+export default Banner;
